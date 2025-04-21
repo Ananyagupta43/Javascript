489409b85d3d755624e1f32397832ba4f06d367c
Ananya Gupta <71815000+Ananyagupta43@users.noreply.github.com>
2025-04-21 06:39:49 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score="33abc";
+let score2=null;
+
+console.log(typeof score);
+
+let valueInNum=Number(score);
+let valueScore2=Number(score2);
+console.log(valueScore2)
+console.log( typeof valueInNum)
+console.log(valueInNum)
+
+// "33" => 33
+// "33abs"=> NaN
+//undefined=> NaN
+//null => 0
+//true => 1
+
+// for boolean type 
+
+//1=> true 0=> false
+// "" => false
+//"hitesh" => true
